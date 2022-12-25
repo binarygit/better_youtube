d6d3aed9ecf4d136a39a6b646b0b50f9ad8888c9
RanjanKhadka012 <112820130+RanjanKhadka012@users.noreply.github.com>
2022-12-25 19:33:19 +0545

    added js codes to remove thumbnails and recommendations

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-// Write the code here
+window.addEventListener("load", function() {
+
+  let thumbnail = document.querySelectorAll("ytd-thumbnail");
+  thumbnail.forEach(function(photo){
+    photo.remove()
+  })
+});
+setInterval(function(){
+    const recommendations = document.getElementById("secondary")
+    recommendations.remove()
+},100);
