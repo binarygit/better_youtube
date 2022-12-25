51ffd32c96c22400c6cb273b3d136022d3dc3876
binarygit <inscription4@outlook.com>
2022-12-25 17:38:05 +0545

    Remove thumbnails and channel avatars in the homepage

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 // Write the code here
+
+function removeThumbnails() {
+  let thumbnails = document.querySelectorAll("ytd-thumbnail");
+  let channelAvatars = document.querySelectorAll("yt-img-shadow");
+  thumbnails.forEach((item) => item.remove());
+  channelAvatars.forEach((item) => item.remove());
+  addMutationObserver();
+}
+
+function addMutationObserver() {
+  const targetNode = document.getElementById("contents");
+  const config = { childList: true, subtree: true };
+  const callback = (mutationList, observer) => {
+    for (const mutation of mutationList) {
+      mutation.addedNodes.forEach((item) => {
+        if (item.tagName == "YTD-RICH-GRID-ROW") {
+          let thumbnails = item.querySelectorAll("ytd-thumbnail");
+          let channelAvatars = document.querySelectorAll("yt-img-shadow");
+          thumbnails.forEach((item) => item.remove());
+          channelAvatars.forEach((item) => item.remove());
+        }
+      });
+    }
+  };
+  const observer = new MutationObserver(callback);
+  observer.observe(targetNode, config);
+}
+
+setTimeout(removeThumbnails, 3000);
